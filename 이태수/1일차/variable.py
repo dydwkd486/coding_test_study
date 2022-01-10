3756c8f6fd42941678fadc15488f2ee4fac77e4c
Taesoooo <89787738+Taesoooo@users.noreply.github.com>
2022-01-10 21:58:23 +0900

    1일차
    1일차

---
+++ 이태수/1일차/variable.py
@@ -0,0 +1,35 @@
+indentity = '지구인'
+number_of_legs = 4
+print('안녕! 나는', indentity, '이야,나는 다리가', number_of_legs, '개 있어.')
+
+indentity = '한국인'
+number_of_legs = 2
+print('안녕! 나는', indentity, '이야,나는 다리가', number_of_legs, '개 있어.')
+
+name = '이태수'
+print(name)
+
+season='겨울'
+print('지금은', season, '입니다.')
+"""
+주석입니다
+"""
+# 주석
+
+my_name = '이태수'
+my_age = 26
+print(my_name, '은 이제', my_age, '살')
+my_next_age = my_age+1
+print(my_name, '은 이제', my_next_age, '살')
+
+multiyply = 9 * 9
+divide = 30/5
+power = 2 ** 10
+reminder = 15 % 4
+print(multiyply, divide, power, reminder)
+
+text = '2022' + '1996'
+number = 2022pwo + 1996
+print(text)
+print(number)
+
